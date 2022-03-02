800ba40c4b6b162b206dee8641274379465ede34
hospice tanguy <htcvhs971@gmail.com>
2022-03-01 22:20:33 -0400

    About/Header/Footer/Skills/Work/Testimonials file structure and link in ./src/container/index.js for ./src/App.js

---
+++ frontend_react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { About, Footer, Header, Skills, Testimonial, Work} from './container';
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
